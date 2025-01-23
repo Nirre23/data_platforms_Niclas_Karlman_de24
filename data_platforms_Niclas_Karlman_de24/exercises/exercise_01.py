df4db994628867b4df1bfedad003d4878d0f2b4f
Nirre23 <niclas.karlman@hotmail.com>
2025-01-23 14:29:20 +0100

    fixed

---
+++ data_platforms_Niclas_Karlman_de24/exercises/exercise_01.py
@@ -0,0 +1,19 @@
+person = {
+    "name": "Niclas",
+    "Age" : 30,
+    "job" : False
+}
+person2 = {
+    "name": "Alica",
+    "age" : 5,
+    "job" : False
+}
+person3 = {
+    "name" : "Josefine",
+    "age" : 30,
+    "job" : "Test Engineer"
+}
+lista = [person,person2,person3]
+print(lista)
+
+
